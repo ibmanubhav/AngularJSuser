8d82dda9d68649185993c23f5a1171c6b1d5630c
unknown <ibmjavanh08@iiht.tech>
2021-04-19 10:59:51 +0530

    Added Delete Button

--- user-service.ts
+++ user-service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { User } from './userform/User';
-
+const URL='http://localhost:8081/user';
 @Injectable({
   providedIn: 'root'
 })
@@ -9,7 +9,16 @@ export class UserService{
 
   constructor(private http: HttpClient) { }
   save(user:User){
-    return this.http.post('http://localhost:8081/user', user, { headers: { "content-type":'application/json'}});
+    return this.http.post(URL, user, {
+      headers: { "content-type":'application/json'}
+    });
     this.http.post('/user', user);
   }
+  getAllUsers(){
+    return this.http.get(URL);
+  }
+  delete(userid : number){
+    return this.http.delete(URL+'/'+userid);
+  }
+
 }
--- userform/userform.component.ts
+++ userform/userform.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from './User';
 import { UserService } from '../user-service';
+import { observable } from 'rxjs';
 
 @Component({
   selector: 'app-userform',
@@ -8,29 +9,31 @@ import { UserService } from '../user-service';
   styleUrls: ['./userform.component.css']
 })
 export class UserformComponent implements OnInit { //controller
-  title:string = 'Userform';
-  user:User= new User(); // model -stores all form of data
-  userArray:User[]=[];
+  title: string = 'Userform';
+  user: User = new User(); // model -stores all form of data
+  userArray: any;
   //age:number=25;
-  constructor(private userService:UserService) { }
-  save(){
-    const promise = this.userService.save(this.user);
-    promise.subscribe(response=> {
-      console.log(response)
+  constructor(private userService: UserService) { }
+  deleteUser(id:number, index:number){
+    const observable = this.userService.delete(id)
+    observable.subscribe(response=> this.userArray.splice(index,1))
+  }
+  save() {
+    const observable = this.userService.save(this.user);
+    observable.subscribe(response => { // SUCCESS FUNCTION
+      console.log(response);
+      this.user.id=response;
       alert('user added')
-      this.userArray.push(Object.assign({},this.user));
+      this.userArray.push(Object.assign({}, this.user));
     },
-    error=> {
-      console.log(error);
-      alert('error happened')
-    })
+      error => {
+        console.log(error);   //ERROR FUNCTION
+        alert('error happened')
+      })
 
-
-
-
-  //  console.log('working');
-  // console.log(this.user.firstname);
-  //this.user.firstname='John';
+    //  console.log('working');
+    // console.log(this.user.firstname);
+    //this.user.firstname='John';
   }
 
   // save1(){
@@ -38,6 +41,12 @@ export class UserformComponent implements OnInit { //controller
   // }
 
   ngOnInit(): void {
+    const observable = this.userService.getAllUsers();
+    observable.subscribe(response => {
+      console.log(response);
+      this.userArray = response;
+
+    });
   }
 
 }
